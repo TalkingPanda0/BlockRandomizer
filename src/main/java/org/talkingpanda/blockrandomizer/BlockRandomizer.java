ecacd000c390fa92c84a119f8869d036b3089a06
Talking Panda <memretwitch3626@gmail.com>
2023-03-24 18:27:33 +0300

    Randomizing

--- src/main/java/org/talkingpanda/blockrandomizer/BlockRandomizer.java
+++ src/main/java/org/talkingpanda/blockrandomizer/BlockRandomizer.java
@@ -3,21 +3,48 @@ package org.talkingpanda.blockrandomizer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 
+import java.util.ArrayList;
+
+import org.joml.Math;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BlockRandomizer implements ModInitializer {
+	
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("org.talkingpanda.blockrandomizer");
+	public static Boolean isActive = false;
+	private static ArrayList<Integer> choices = new ArrayList<Integer>();
+	private static int multipliers[] = new int[]{1,1,1,1,5,1,1,1,1};
 
+	private static MinecraftClient client;
 	private static KeyBinding KeyBinding; 
+	public static void RandomizeBlock(ClientPlayerEntity player)
+	{
+		PlayerInventory inventory =  player.getInventory();
+		inventory.selectedSlot = choices.get((int)(Math.random() * choices.size()));
+	}
+	private static void GenerateChoices(){
+		choices. clear();
+		for(int i=0;i<multipliers.length;i++)
+		{
+			for(int ii=0;ii<multipliers[i];ii++)
+			{
+				choices.add(i);
+			}
+		}
+
+	}
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -26,14 +53,20 @@ public class BlockRandomizer implements ModInitializer {
 		KeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 					"key.blockrandomizer.start",
 					InputUtil.Type.KEYSYM,
-					GLFW.GLFW_KEY_R,
+					GLFW.GLFW_KEY_F4,
 					"category.talkingpanda.start"
 				
 					)
 				);
+		client = MinecraftClient.getInstance();
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while(KeyBinding.wasPressed()) {
-				client.player.sendMessage(Text.literal("Key 1 was pressed!"), false);
+				
+				isActive = !isActive;
+				client.inGameHud.setTitle(Text.literal(isActive ? "Block Randomizer: ON" : "Block Randomizer: OFF"));
+				if(isActive)
+					GenerateChoices();
+				LOGGER.info(choices.toString());;
 			}
 		});
 		
--- src/main/java/org/talkingpanda/blockrandomizer/mixin/BlockRandomizerMixin.java
+++ src/main/java/org/talkingpanda/blockrandomizer/mixin/BlockRandomizerMixin.java
@@ -1,16 +1,25 @@
 package org.talkingpanda.blockrandomizer.mixin;
 
 import org.talkingpanda.blockrandomizer.BlockRandomizer;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult.Type;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(TitleScreen.class)
+@Mixin(ClientPlayerInteractionManager.class)
 public class BlockRandomizerMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		BlockRandomizer.LOGGER.info("This line is printed by an example mod mixin!");
-	}
+	
+    @Inject(method="interactBlock", at=@At(value="RETURN",
+            target="Lnet/minecraft/client/network/ClientPlayerInteractionManager;sendSequencedPacket(Lnet/minecraft/client/world/ClientWorld;Lnet/minecraft/client/network/SequencedPacketCreator;)V"))
+    public void onPlayerInteractBlockSuccessfully(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable cir) {
+	    // TODO run only when a block placed
+	    if(BlockRandomizer.isActive && hitResult.getType().equals(Type.BLOCK))
+		BlockRandomizer.RandomizeBlock(player);
+    }
 }
